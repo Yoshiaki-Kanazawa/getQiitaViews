af514a70b92ac7c67f2847e7e333e508cd98f529
Yoshiaki-Kanazawa <k.yoshiaki1226@gmail.com>
2020-06-22 22:51:13 +0900

    データ保存部分追加

--- Constants.cs
+++ Constants.cs
@@ -6,5 +6,6 @@ namespace kanazawa.Function
     {
         public const string Qiita_Access_Token = "9f7be102bcb181234c6763d92bdf053e6b205211";
         public const string Qiita_User_Name = "SE_AmericanFootball";
+        public const string Conection_String = "Server=tcp:functions.database.windows.net,1433;Initial Catalog=qiita;Persist Security Info=False;User ID=sqladmin;Password=Yoshiaki1226;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
--- get-qiita-views.cs
+++ get-qiita-views.cs
@@ -7,13 +7,14 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace kanazawa.Function
 {
     public static class get_qiita_views
     {
         [FunctionName("get_qiita_views")]
-        public static async void Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
+        public static async void Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
@@ -40,6 +41,65 @@ namespace kanazawa.Function
                 model.PageViewsCount = JsonConvert.DeserializeObject<QiitaInformationModel>(await GetJson(getViewsCountUrl)).PageViewsCount;
                 log.LogInformation($"title: {model.Title}");
                 log.LogInformation($"views: {model.PageViewsCount}");
+            }
+
+            // DB接続文字列の取得
+            var connectionString = Constants.Conection_String;
+
+            // データ保存
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // データベースの接続開始
+                    connection.Open();
+
+                    using (var transaction = connection.BeginTransaction())
+                    {
+                        try
+                        {
+
+                            foreach (var model in models)
+                            {
+                                using (var command = new SqlCommand() { Connection = connection, Transaction = transaction })
+                                {
+                                    // SQLの準備
+                                    command.CommandText = @"INSERT INTO page_views_count VALUES (@ID, @CREATED_AT, @TITLE, @PAGE_VIEWS_COUNT)";
+                                    command.Parameters.Add(new SqlParameter("@ID", model.Id));
+                                    command.Parameters.Add(new SqlParameter("@CREATED_AT", DateTime.Now.ToString("yyyy/MM/dd HH")));
+                                    command.Parameters.Add(new SqlParameter("@TITLE", model.Title));
+                                    command.Parameters.Add(new SqlParameter("@PAGE_VIEWS_COUNT", model.PageViewsCount));
+
+                                    // SQLの実行
+                                    command.ExecuteNonQuery();
+
+                                    log.LogInformation($"succeeded to insert data: {model.Title}");
+                                }
+                            }
+
+                            // コミット
+                            transaction.Commit();
+                            log.LogInformation("Committed");
+                        }
+                        catch
+                        {
+                            // ロールバック
+                            transaction.Rollback();
+                            log.LogInformation("Rollbacked");
+                            throw;
+                        }
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    throw;
+                }
+                finally
+                {
+                    // データベースの接続終了
+                    connection.Close();
+                }
             }
 
         }
